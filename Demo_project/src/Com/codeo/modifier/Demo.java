151676d06c001a2c27ca7aab3929b62c3a4e8155
Mitali <LehagaonkarMukta@gmail.com>
2024-05-22 03:02:19 -0700

    Hey I have commitedthis code

---
+++ Demo_project/src/Com/codeo/modifier/Demo.java
@@ -0,0 +1,10 @@
+package Com.codeo.modifier;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Demo_project/src/Com/codeo/modifier/Modifier.java
@@ -0,0 +1,11 @@
+package Com.codeo.modifier;
+
+public class Modifier {
+	//private variable accessible for only this class
+	private int a =12;
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
